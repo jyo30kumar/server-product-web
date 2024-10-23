3f10c183897bba44f0e1d8d9387a6a9ce4a70220
kumar <jyotishgehlot@gmail.com>
2024-10-23 18:29:33 +0530

    new setup for database

--- config/db-config.js
+++ config/db-config.js
@@ -1,14 +1,37 @@
 import { Sequelize } from "sequelize";
 
+// for production development
+const DB_NAME = process.env.DB_NAME_DEV;
+const DB_USERNAME = process.env.DB_USERNAME_DEV;
+const DB_PASSWORD = process.env.DB_PASSWORD_DEV; 
+const DB_HOST = process.env.DB_HOST_DEV;
+const DB_DIALECT = process.env.DB_DIALECT_DEV;
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER_NAME,
-  process.env.DB_PASSWORD,
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
     logging:false,
   }
 );
+
+// for production 
+// const DB_NAME = process.env.DB_NAME_PROD;
+// const DB_USERNAME = process.env.DB_USERNAME_PROD;
+// const DB_PASSWORD = process.env.DB_PASSWORD_PROD; 
+// const DB_HOST = process.env.DB_HOST_PROD;
+// const DB_DIALECT = process.env.DB_DIALECT_PROD;
+// const db = new Sequelize(
+//   DB_NAME,
+//   DB_USERNAME,
+//   DB_PASSWORD,
+//   {
+//     host: DB_HOST,
+//     dialect: DB_DIALECT,
+//     logging:false,
+//   }
+// );
 
 export { db };
---
+++ config/sequelize-cli-config.cjs
@@ -0,0 +1,37 @@
+const DB_NAME_DEV = process.env.DB_NAME_DEV;
+const DB_USERNAME_DEV = process.env.DB_USERNAME_DEV;
+const DB_PASSWORD_DEV = process.env.DB_PASSWORD_DEV;
+const DB_HOST_DEV = process.env.DB_HOST_DEV;
+const DB_DIALECT_DEV = process.env.DB_DIALECT_DEV;
+
+const DB_NAME_PROD = process.env.DB_NAME_PROD;
+const DB_USERNAME_PROD = process.env.DB_USERNAME_PROD;
+const DB_PASSWORD_PROD = process.env.DB_PASSWORD_PROD;
+const DB_HOST_PROD = process.env.DB_HOST_PROD;
+const DB_DIALECT_PROD = process.env.DB_DIALECT_PROD;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME_DEV,
+    password: DB_PASSWORD_DEV,
+    database: DB_NAME_DEV,
+    host: DB_HOST_DEV,
+    dialect: DB_DIALECT_DEV,
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'Migrations',
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'Seeders',
+  },
+  production: {
+    username: DB_USERNAME_PROD,
+    password: DB_PASSWORD_PROD,
+    database: DB_NAME_PROD,
+    host: DB_HOST_PROD,
+    dialect: DB_DIALECT_PROD,
+    port: process.env.DB_PORT_PROD,
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'Migrations',
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'Seeders',
+  },
+};
